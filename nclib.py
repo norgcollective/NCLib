c6fc408d9b0cd4c53329fca22ce2b19329a8972c
heschy2 <105609148+heschy2@users.noreply.github.com>
2023-02-07 22:40:25 +0000

    Add files via upload

--- nclib.py
+++ nclib.py
@@ -1,4 +1,4 @@
-import gi;
+import time, gi;
 gi.require_version("Gtk","4.0");
 gi.require_version("Adw", "1");
 from gi.repository import Gtk, Adw;
@@ -35,26 +35,45 @@ def SubAndReset(min, max, first, second): # if a-b < min -> max - ( b - a )
 
     return result
 
-def CreateAdwApplicationWindow(app, title, edition, devel_mode=False,silent=False):
-        if not silent: print("NCLIB: Called NCLIB.CreateAdwApplicationWindow")
-        MainWindow = Adw.ApplicationWindow(application=app)
-        if devel_mode: MainWindow.add_css_class('devel')
+def CreateAdwApplicationWindow(self, app, title="Window", edition="Stable", devel_mode=False,silent=False,clamp=False,maxsize=750, maxheight=False):
+        if not silent: print("NCLIB [" + str(time.time()).split('.')[0] + "]: Called NCLIB.CreateAdwApplicationWindow")
+        self.MainWindow = Adw.ApplicationWindow(application=app)
+        if devel_mode: self.MainWindow.add_css_class('devel')
 
-        WindowContent   = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 
-        widget_TitleBar = Adw.HeaderBar()
-        widget_Title    = Adw.WindowTitle(title=title, subtitle=edition)
+        self.WindowContent_Full   = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 
-        WindowContent.append(widget_TitleBar)
+        self.widget_TitleBar = Adw.HeaderBar()
+        self.widget_Title    = Adw.WindowTitle(title=title, subtitle=edition)
 
-        widget_TitleBar.set_title_widget(widget_Title)
+        self.WindowContent_Full.append(self.widget_TitleBar)
 
-        return (MainWindow, WindowContent)
-        ## How To Use: (of course, in Adw.Appication Class)
-        
-        #cache = NCLib.CreateAdwApplicationWindow(app, "Project Rain", "Developer Edition", devel_mode=True)
-        #self.MainWindow    = cache[0]
-        #self.WindowContent = cache[1]
-        #del cache
+        self.widget_TitleBar.set_title_widget(self.widget_Title)
 
-        #self.MainWindow.set_content(self.WindowContent) ## THIS IS IMPORTANT
+        self.MainWindow.set_content(self.WindowContent_Full)
+
+        self.WindowContent = Adw.ToastOverlay(margin_bottom=10,margin_top=10,margin_start=10,margin_end=10)
+        self.WindowContent_Full.append(self.WindowContent)
+
+        self.ClampLayout = Adw.ClampLayout(maximum_size=maxsize)
+
+        if clamp:
+            self.WindowContent.set_layout_manager(self.ClampLayout)
+
+        return "Added MainWindow, WindowContent, widget_TitleBar and widget_Title to self"
+
+def CreateAdwAboutWindow(self, AppName, AppVersion, Comment, AppWebsite, DeveloperArray, IconID, silent=False):
+    if not silent: print("NCLIB [" + str(time.time()).split('.')[0] + "]: Called NCLIB.CreateAdwAboutWindow")
+    dialog = Adw.AboutWindow(transient_for=self.MainWindow)
+    dialog.set_application_name(AppName)
+    dialog.set_version(AppVersion)
+    dialog.set_developer_name("Norg Collective")
+    dialog.set_license_type(Gtk.License(Gtk.License.GPL_3_0))
+    dialog.set_comments(Comment)
+    dialog.set_website("https://" + AppWebsite)
+    dialog.set_issue_url("https://" + AppWebsite + "/issues")
+    dialog.set_developers(DeveloperArray)
+    dialog.set_application_icon(IconID)
+
+    self.dlg_AboutApplication = dialog
+    return "Added dlg_AboutApplication to self"
