2ac03313c5d553af861aff2d0068f64824ed03ee
heschy2 <105609148+heschy2@users.noreply.github.com>
2023-02-09 15:36:42 +0000

    Update nclib.py

--- nclib.py
+++ nclib.py
@@ -35,15 +35,18 @@ def SubAndReset(min, max, first, second): # if a-b < min -> max - ( b - a )
 
     return result
 
-def CreateAdwApplicationWindow(self, app, title="Window", edition="Stable", devel_mode=False,silent=False,clamp=False,maxsize=750, maxheight=False):
-        if not silent: print("NCLIB [" + str(time.time()).split('.')[0] + "]: Called NCLIB.CreateAdwApplicationWindow")
+def CreateAdwApplicationWindow(self, app, title="Window", edition="Stable", devel_mode=False,silent=False,clamp=False,maxsize=750, maxheight=False, flat=False, resize=True, talking=False):
+        if (not silent) and (not talking): print("NCLIB [" + str(time.time()).split('.')[0] + "]: Called NCLIB.CreateAdwApplicationWindow")
+        if (not silent) and talking: print("NCLIB [" + str(time.time()).split('.')[0] + "]: Called NCLIB.CreateAdwApplicationWindow(Adw.Application, '" + str(title) + "', '" + str(edition) + "', " + str(devel_mode) + ", " + str(silent) + ", " + str(clamp) + ", " + str(maxsize) + ", " + str(maxheight) + ", " + str(flat) + ", " + str(resize) + ", " + str(talking) + ")")
         self.MainWindow = Adw.ApplicationWindow(application=app)
-        if devel_mode: self.MainWindow.add_css_class('devel')
-
+        self.is_under_devel = devel_mode
+        if self.is_under_devel: self.MainWindow.add_css_class('devel')
+        self.MainWindow.set_resizable(resize)
 
         self.WindowContent_Full   = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 
         self.widget_TitleBar = Adw.HeaderBar()
+        if flat: self.widget_TitleBar.add_css_class('flat')
         self.widget_Title    = Adw.WindowTitle(title=title, subtitle=edition)
 
         self.WindowContent_Full.append(self.widget_TitleBar)
@@ -65,6 +68,7 @@ def CreateAdwApplicationWindow(self, app, title="Window", edition="Stable", deve
 def CreateAdwAboutWindow(self, AppName, AppVersion, Comment, AppWebsite, DeveloperArray, IconID, silent=False):
     if not silent: print("NCLIB [" + str(time.time()).split('.')[0] + "]: Called NCLIB.CreateAdwAboutWindow")
     dialog = Adw.AboutWindow(transient_for=self.MainWindow)
+    if self.is_under_devel: dialog.add_css_class('devel')
     dialog.set_application_name(AppName)
     dialog.set_version(AppVersion)
     dialog.set_developer_name("Norg Collective")
@@ -76,4 +80,13 @@ def CreateAdwAboutWindow(self, AppName, AppVersion, Comment, AppWebsite, Develop
     dialog.set_application_icon(IconID)
 
     self.dlg_AboutApplication = dialog
-    return "Added dlg_AboutApplication to self"
+
+    if not bool(self.widget_InfoBtn):
+        self.widget_InfoBtn = Gtk.Button.new_from_icon_name('dialog-information-symbolic')
+        self.widget_InfoBtn.add_css_class('flat')
+        self.widget_InfoBtn.add_css_class('circular')
+        self.widget_InfoBtn.connect("clicked", self.on_about)
+
+        self.widget_TitleBar.pack_end(self.widget_InfoBtn)
+        return "Added dlg_AboutApplication and widget_InfoBtn to self"
+    return "Added dlg_AboutApplication self"
